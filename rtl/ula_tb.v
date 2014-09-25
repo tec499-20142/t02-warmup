6d519e6077a8e5afec05d5de09da0cc3554ff0d3
Diego Leite <diegojleite@gmail.com>
2014-09-25 15:29:18 -0300

    Apresentação e tudo funcionando

--- rtl/ula_tb.v
+++ rtl/ula_tb.v
@@ -1,50 +1,46 @@
-module ula_tb;
+module ula(
+	input [7:0] operator1,
+	input [7:0] operator2,
+	input [7:0] operation_alu,
+	output reg [7:0] result_alu,
+	output reg overflow
+);
 
-	reg[7:0] op1, op2, res, code;
-	wire[7:0] r;
-	wire of;
-	integer f;
 
-	ula_k u(
-	   .operator1(op1),
-		.operator2(op2),
-		.operation_alu(code),
-		.overflow(of),
-		.result_alu(r)
-	);
+always @(operator1 or operator2)
+
+begin
 	
-	initial
-	begin
-	  f = $fopen("ula_tb_output.txt");
-	  $fmonitor(f, "Operador1: %d Operador2: %d Resultado: %d Overflow: %b", op1, op2, r, of);
-	  
-	  code = 8'b00000000;
-	  
-	  repeat(6) begin
-	    case(code)
-	      8'b00000000: $fdisplay(f, "Soma");
-	      8'b00000001: $fdisplay(f, "SubtraÁ„o");
-	      8'b00000010: $fdisplay(f, "And");
-	      8'b00000011: $fdisplay(f, "Or");
-	      8'b00000100: $fdisplay(f, "MultiplicaÁ„o");
-	      8'b00000101: $fdisplay(f, "Divis„o");
-	   endcase
-	   /* Sorteia dois n˙meros entre 0 e 255
-	    * para os operadores 1 e 2
-	    */
-	   repeat(100) begin
-	       op1 = $unsigned($random) % 255;
-		     op2 = $unsigned($random) % 255;
-		     #1;
-	   end
-	   // Incrementa o cÛdigo da operaÁ„o
-	   code = code + 1;
-	  end //end repeat
-	  
-	  // Fecha o arquivo
-	  $fclose(f);
-	  
-	  // Termina a simulaÁ„o
-	  $finish;
-	end //end initial
-endmodule	
+	case(operation_alu)
+	
+	8'b00000000: result_alu = operator1 + operator2;
+	8'b00000001: result_alu = operator1 - operator2;
+	8'b00000010: result_alu = operator1 & operator2;
+	8'b00000011: result_alu = operator1 | operator2;
+	8'b00000100: result_alu = operator1 * operator2;
+	8'b00000101: result_alu = operator1 / operator2;
+	
+	endcase
+	
+	//Verifica a opera√ß√£o adi√ß√£o
+	if ((operation_alu == 0 && ((operator1 + operator2) > 255)) || 
+	//Verifica a opera√ß√£o de subtra√ß√£o
+	(operation_alu == 1 && ((operator1 - operator2) < 0)) ||
+	//Verifica a opera√ß√£o de multiplica√ß√£o
+	(operation_alu == 4 && ((operator1 * operator2) > 255)) ||
+	//Verifica se o denominador da divis√£o √© 0 tamb√©m, se for .. dar√° overflow.
+	(operation_alu == 5 && (operator2 == 0||((operator1 / operator2) < 0) 
+	|| (operator1 / operator2) > 255))) overflow =0;
+	
+	
+	else 
+		begin
+
+			overflow =0;
+
+		end
+	
+
+end
+	
+endmodule
